82701c1468d4bb5d16090d40c0cb9bce3258d3ac
pierce.faraone <pierce.faraone@gmail.com>
2021-01-26 12:14:55 +0800

    1x easy stack question

---
+++ blindCurated75+EPI/stacksAndQueues/validParantheses-20-E/main.go
@@ -0,0 +1,42 @@
+package main
+
+func isValid(s string) bool {
+	if len(s) < 2 {
+		return false
+	}
+	bracketDict := map[string]string{")": "(",
+		"}": "{",
+		"]": "["}
+	bracketStack := make([]string, 0)
+	for char := range s {
+		stringVersion := string(char)
+		bracket, in_map := bracketDict[stringVersion]
+		// case : opening bracket -> push onto stack
+		if !in_map {
+			bracketStack = append(bracketStack, stringVersion)
+		} else {
+			// pop off stack most recent pushed on open bracket
+			var lastOpeningBracket string
+			if len(bracketStack) > 0 {
+				lastOpeningBracket = bracketStack[len(bracketStack)-1]
+				bracketStack = bracketStack[:len(bracketStack)-1]
+			} else {
+				lastOpeningBracket = "a"
+			}
+			if bracket != string(lastOpeningBracket) {
+				return false
+			}
+		}
+		// fmt.Println(bracketStack,i,string(char))
+	}
+	// fmt.Println(bracketStack)
+	if len(bracketStack) == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+
+}
